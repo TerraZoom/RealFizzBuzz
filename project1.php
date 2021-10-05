5e1ff573433704bf1d0e2aa76a7c4e7268ef1a9a
John Williams <john@terrazoom.com>
2021-10-04 21:19:46 -0600

    Initial commit

---
+++ project1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head><title>RealFizzBuzz Project 1</title></head>
+<body>
+
+@php
+$title = "My First Project";
+$myArray = ['id'=>1, 'name'=>'Marluk'];
+
+@endphp
+
+<h1>{{ $title }}
+
+
+</body>
+</html>
---
+++ public/project1.php
@@ -0,0 +1,27 @@
+<h1>Project 1</h1>
+
+<?php
+function checkMultiple($multiple, $number) {
+  if($number % $multiple) {
+
+  }
+}
+
+$arr = array(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+
+foreach ($arr as $value) {
+  // echo $value . "-" . $value * 2 . " ";
+
+if($value%15 == 0) {
+    echo "fizzbuzz";
+  } else if ($value % 5 == 0) {
+    echo "buzz";
+  } else if ($value % 3 == 0) {
+    echo "fizz";
+} else {
+    echo $value;
+  }
+}
+
+
+?>
